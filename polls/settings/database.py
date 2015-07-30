03285c28a6d02b472b6f5dbaa4727a21f1756730
Pankaj Singh <psjinx@gmail.com>
2015-07-30 18:12:41 +0530

    Use URL strings for database and redis config
    Signed-off-by: Pankaj Singh <psjinx@gmail.com>

--- polls/settings/database.py
+++ polls/settings/database.py
@@ -4,44 +4,13 @@ import os
 # Third Party Stuff
 from sqlalchemy import create_engine
 
-# string format for connection url
-CONNECTION_URL_FMT = 'postgresql://{user}:{password}@{host}:{port}/{name}'
-
-
-def get_db_connection_dict_from_env():
-    """
-    Build a dict of connection arguments from the environment.
-
-    :return:
-    """
-    # get settings from envornment variables
-    db_name = os.environ.get('DB_NAME')
-    db_user = os.environ.get('DB_USER')
-    db_password = os.environ.get('DB_PASSWORD')
-    db_host = os.environ.get('DB_HOST')
-    db_port = os.environ.get('DB_PORT')
-
-    return dict(
-        name=db_name,
-        user=db_user,
-        password=db_password,
-        host=db_host,
-        port=db_port,
-    )
-
 
 def get_db_connection_url():
     """
     Return database connection URL.
     It's used by sqlalchemy.
     """
-    # get database connection settings as a dictionary
-    db_kwargs = get_db_connection_dict_from_env()
-
-    # create connection url using format and db_kwargs
-    connection_url = CONNECTION_URL_FMT.format(**db_kwargs)
-
-    return connection_url
+    return os.environ["DATABASE_URL"]
 
 
 def get_db_engine():
--- polls/settings/redis.py
+++ polls/settings/redis.py
@@ -3,14 +3,4 @@ import os
 
 
 def get_redis_url():
-    redis_db_name = os.environ.get('REDIS_NAME')
-    redis_host = os.environ.get('REDIS_HOST')
-    redis_port = os.environ['REDIS_PORT']
-
-    url = 'redis://{host}:{port}/{name}'.format(
-        name=redis_db_name,
-        host=redis_host,
-        port=redis_port,
-    )
-
-    return url
+    return os.environ["REDIS_URL"]
